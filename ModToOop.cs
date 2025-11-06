0ad6483894da551ec5cf644e4ec50bff875bd8e8
Osss <Osamaabuzanh@gami.com>
2025-11-06 08:20:45 +0300

    TestOop

---
+++ ModToOop.cs
@@ -0,0 +1,39 @@
+﻿
+namespace NumAvg
+{
+
+    public class Greater
+    {
+        public int[] Arr(int[] arr)
+
+        {
+            var sum = 0;
+            var average = 0;
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+                sum += arr[i];
+                average = sum / arr.Length;
+
+            }
+
+            Console.WriteLine($"TheAverage={average}");
+
+
+            for (int x = 0; x < arr.Length; x++)
+            {
+                if (arr[x] > average)
+                {
+                    Console.WriteLine($"TheNumberGreaterThanAvg={arr[x]}");
+                }
+            }
+            return arr;
+
+        }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,24 +1,7 @@
 ﻿
-int[] arr = new int[5];
-var sum = 0;
-var average = 0.0;
-
-for (int i = 0; i < 5; i++)
-{
-    Console.WriteLine("Enter the Element {0}", i + 1);
-    arr[i] = Convert.ToInt32(Console.ReadLine());
-    sum += arr[i];
-   
-}
-average = (double)sum / arr.Length;
-
-Console.WriteLine("Elements greater than Average:");
-    for (int i = 0; i < arr.Length; i++)
-    
-        if (arr[i] > average)
-        {
-            Console.WriteLine(arr[i]);
-        }
-
-Console.WriteLine("Average of all Elements is: {0}", average); 
+using NumAvg;
+Greater grt = new Greater();
+Console.WriteLine($"Enter your Number");
+int[] arr = new int[3];
+int[] avg = grt.Arr(arr);
 
